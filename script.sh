56305b2421e005f19913ef3f752066479720e611
developer-coder117 <52752684+developer-coder117@users.noreply.github.com>
2019-08-14 16:43:22 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@ for i in $(cat sources.txt)
 do
 git clone $i
 done
-ls | grep cavca> files.txt
+ls | grep cavca > files.txt
 for f in $(cat files.txt)
 do
 cd $f && npm i && npm run compile && cd ..
